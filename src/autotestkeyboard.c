30e81938debd45b0ed196b2bc77cb5f7df60016c
Sampo Peltonen <sampo.peltonen@gmail.com>
2013-09-30 22:51:37 +0300

    project name change

--- src/autotestkeyboard.c
+++ src/autotestkeyboard.c
@@ -14,7 +14,7 @@ int finished = 0;
 int shifted = 0;
 
 //const char text[] = "abcdefghijklm\\nopqrst\\uvxyz 1234567890";
-//const char text[] = "print\\2\\raspi64 rules ok\\2\\  \n\n" "20 i=5 \n\n" "run\n";
+//const char text[] = "print\\2\\pi64 rules ok\\2\\  \n\n" "20 i=5 \n\n" "run\n";
 /*
 const char* text = 	"print 1+3\n"
 			"10 i=64\n"
@@ -24,7 +24,7 @@ const char* text = 	"print 1+3\n"
 
 const char* text =
 "10 i=0\n"                                  
-"20 print\\2\\raspi64 rules ok\\12\\\n"             
+"20 print\\2\\pi64 rules ok\\12\\\n"             
 "30 i=i+1\n"                                
 "40 ifi\\,\\10thengoto20\n"                     
 "run\n";
